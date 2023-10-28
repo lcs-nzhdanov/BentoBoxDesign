7e27f71e568188d17424d30594df7269fffd75f9
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2023-10-28 12:30:33 -0400

    I have added text into first 3 boxes, created a gradient for the text color.

--- BentoBoxDesign/ContentView.swift
+++ BentoBoxDesign/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
             let mediumGray = Color(
                 hue: 0/360.0,
                 saturation: 0.0,
-                brightness: 0.8
+                brightness: 0.45
             )
             let darkGray = Color(
                 hue: 0/360.0,
@@ -33,7 +33,7 @@ struct ContentView: View {
             let mediumPurple = Color(
                 hue: 266.0/360.0,
                 saturation: 0.35,
-                brightness: 0.50
+                brightness: 0.7
             )
             let darkPurple = Color(
                 hue: 286.0/360.0,
@@ -44,18 +44,60 @@ struct ContentView: View {
         
         let blackSquareGradientColors = Gradient(colors: [Color.black, darkGray])
         let purpleMaxGradientColors = Gradient(colors: [darkPurple, lightPurple])
+        let m2GradientColors = Gradient(colors: [mediumGray, lightGray])
+        let textGradientColor = Gradient(colors: [mediumPurple, darkPurple])
         
         let squareGradient = LinearGradient(gradient: blackSquareGradientColors, startPoint: .bottomTrailing, endPoint: .leading)
         let maxGradient = LinearGradient(gradient: purpleMaxGradientColors, startPoint: .topTrailing, endPoint: .bottomLeading)
+        let m2Gradient = LinearGradient(gradient: m2GradientColors, startPoint: UnitPoint(x: 0, y: 0.6), endPoint: UnitPoint(x: 1, y: 0.4))
+        let textGradient = LinearGradient(gradient: textGradientColor, startPoint: UnitPoint(x: 0, y: 0.5), endPoint: UnitPoint(x: 1, y: 0.5))
         
         
         HStack {
             VStack {
                 HStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(.gray)
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(.gray)
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.gray)
+                        
+                        VStack {
+                            Image("Thunderbolt")
+                                  .resizable()
+                                  .scaledToFit()
+                              .frame(width: 100)
+                            
+                            Text("Thunderbolt 4")
+                                .foregroundStyle(Color.black)
+                                .bold()
+                            
+                            Spacer()
+                        }
+                    }
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.gray)
+                        
+                        VStack {
+                            Spacer()
+                            
+                            Text("Up to")
+                                .foregroundStyle(Color.black)
+                                .bold()
+                            
+                            Text("20%")
+                                .foregroundStyle(textGradient)
+                                .font(.system(size: 44))
+                                .bold()
+                                .frame(height: 60)
+                            
+                            Text("faster CPU")
+                                .foregroundStyle(Color.black)
+                                .bold()
+                            
+                            Spacer()
+                        }
+                    }
                         
                 }
                 .frame(height: 122.5)
@@ -84,11 +126,46 @@ struct ContentView: View {
             
             VStack {
                 HStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(.gray)
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.gray)
                         .frame(width: 120)
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(.gray)
+                        
+                        VStack {
+                            Spacer()
+                            
+                            Text("Up to")
+                                .foregroundStyle(Color.black)
+                                .bold()
+                            
+                            Text("30%")
+                                .foregroundStyle(textGradient)
+                                .font(.system(size: 44))
+                                .bold()
+                                .frame(height: 60)
+                            
+                            Text("faster GPU")
+                                .foregroundStyle(Color.black)
+                                .bold()
+                            
+                            Spacer()
+                        }
+                    }
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.gray)
+                        
+                        Text("""
+                             Industry-leading
+                             perfomance
+                             per watt
+                             """)
+                        .foregroundStyle(textGradient)
+                        .font(.system(size: 20))
+                        .multilineTextAlignment(.center)
+                        
+                    }
                 }
                 .frame(height: 122.5)
                 
@@ -107,6 +184,7 @@ struct ContentView: View {
                         VStack(alignment: .center){
                             Text("M2")
                                 .font(.system(size: 60))
+                                .foregroundStyle(m2Gradient)
                             .frame(width: 150, height: 40)
                             
                             Text("MAX")
