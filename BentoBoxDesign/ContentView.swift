182d73a1079a589d5328d09a8724ac482c0dccbf
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2023-10-27 23:22:00 -0400

    I have created a full block structure, as it is on the picture. I have added central square and text. There are some usefull colors in the project and created gradients with them

--- BentoBoxDesign/ContentView.swift
+++ BentoBoxDesign/ContentView.swift
@@ -9,54 +9,140 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        let lightGray = Color(
+                hue: 0/360.0,
+                saturation: 0.0,
+                brightness: 0.9
+            )
+            let mediumGray = Color(
+                hue: 0/360.0,
+                saturation: 0.0,
+                brightness: 0.8
+            )
+            let darkGray = Color(
+                hue: 0/360.0,
+                saturation: 0.0,
+                brightness: 0.2
+            )
+            let lightPurple = Color(
+                hue: 266.0/360.0,
+                saturation: 0.15,
+                brightness: 1.0
+            )
+            let mediumPurple = Color(
+                hue: 266.0/360.0,
+                saturation: 0.35,
+                brightness: 0.50
+            )
+            let darkPurple = Color(
+                hue: 286.0/360.0,
+                saturation: 0.65,
+                brightness: 0.50
+            )
+        
+        
+        let blackSquareGradientColors = Gradient(colors: [Color.black, darkGray])
+        let purpleMaxGradientColors = Gradient(colors: [darkPurple, lightPurple])
+        
+        let squareGradient = LinearGradient(gradient: blackSquareGradientColors, startPoint: .bottomTrailing, endPoint: .leading)
+        let maxGradient = LinearGradient(gradient: purpleMaxGradientColors, startPoint: .topTrailing, endPoint: .bottomLeading)
+        
+        
         HStack {
             VStack {
                 HStack {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(.gray)
-                        VStack {
-                            Spacer()
-                            
-                            //Image("Thunderbolt")
-                            Image(systemName: "cloud.bolt.rain")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 125)
-                                .colorInvert()
-                            Spacer()
-                            
-                            Text("Thunderbolt 4")
-                            
-                            Spacer()
-                                .frame(height: 10)
-                        }
-                    }
-                    
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.gray)
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.gray)
+                        
+                }
+                .frame(height: 122.5)
+                
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.gray)
+                
+                
+                HStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.gray)
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.gray)
+                }
+                .frame(height: 122.5)
+                
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.gray)
+                    .frame(height: 85)
+            }
+            .frame(width: 240)
+            
+            
+            
+            VStack {
+                HStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.gray)
+                        .frame(width: 120)
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.gray)
+                }
+                .frame(height: 122.5)
+                
+                
+                ZStack {
                     RoundedRectangle(cornerRadius: 20)
                         .fill(.gray)
                     
                     
-                    
+                    ZStack {
+                        Rectangle()
+                            .fill(squareGradient)
+                            .scaledToFit()
+                        .frame(width: 200)
+                        
+                        VStack(alignment: .center){
+                            Text("M2")
+                                .font(.system(size: 60))
+                            .frame(width: 150, height: 40)
+                            
+                            Text("MAX")
+                                .bold()
+                                .font(.system(size: 30))
+                                .foregroundStyle(maxGradient)
+                                .frame(height: 30)
+                        }
+                    }
                 }
+                
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.gray)
+                    .frame(height: 85)
             }
             
             
             
-            ZStack {
-                Rectangle()
+            VStack {
+                RoundedRectangle(cornerRadius: 20)
                     .fill(.gray)
+                    .frame(height: 122.5)
                 
                 HStack {
-                    Image(systemName: "trash.slash")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 250)
-
-
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.gray)
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.gray)
                 }
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.gray)
+                    .frame(height: 170)
             }
-            
+            .frame(width: 240)
             
         }
         .frame(width: 800, height: 500)
